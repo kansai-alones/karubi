a8cc9d98c27eecddf02acbbe2d75bb48a38fc6df
Yuya Aratani <hrgruri@gmail.com>
2017-09-24 03:58:35 +0900

    アンケートの選択肢のテーブルを追加

---
+++ backend/database/migrations/2017_09_23_182814_choices.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Choices extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('choices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('enquete_id');
+            $table->string('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('choices');
+    }
+}
