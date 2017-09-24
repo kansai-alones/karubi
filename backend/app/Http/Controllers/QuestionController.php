11bd49785d65425270302aa48f6be53e7ee73163
Yuya Aratani <hrgruri@gmail.com>
2017-09-24 11:35:26 +0900

    コントローラの整理

---
+++ backend/app/Http/Controllers/QuestionController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use App\Question;
+use App\Result;
+
+class QuestionController extends Controller
+{
+    /**
+     * コードの評価を行い, データベースに保存する. 結果を返す.
+     * @param  Request $request
+     * @return Result コードの評価
+     */
+    public function create(Request $request) : Result
+    {
+        $user = User::where('token', $request->input('token'))->first();
+        $question = Question::find($request->input('question_id'));
+        $data = $request->input();
+        $data['user_id'] = $user->id;
+        $data['level'] = $question->level;
+
+        // TODO コードテスト
+        $data['check_list'] = [
+            ['flag' => true],
+            ['flag' => false],
+            ['flag' => false],
+            ['flag' => true],
+            ['flag' => true],
+            ['flag' => false],
+            ['flag' => true],
+        ];
+        return Result::calculate($data);;
+    }
+}
